841de9662b20d32b26bf4a46305cca53b5cc62dd
Handy Hasan <hhasan@4dx.com>
2022-07-14 15:57:43 +1000

    Initial Commit

---
+++ BookOfMoons/BookOfMoonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookOfMoonsApp.swift
+//  BookOfMoons
+//
+//  Created by Handy Hasan on 14/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookOfMoonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
